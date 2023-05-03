23a601c4eb253a0d6af09054749cfdb7cc8ea5db
TallTall22 <syuns1205@gmail.com>
2023-05-03 17:48:24 +0800

    build Collection Model

---
+++ migrations/20230503093920-create-collection.js
@@ -0,0 +1,46 @@
+'use strict'
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Collections', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      category: {
+        type: Sequelize.STRING
+      },
+      slogan: {
+        type: Sequelize.STRING
+      },
+      artMaker: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      artRemark: {
+        type: Sequelize.STRING
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Collections');
+  }
+}
---
+++ models/collection.js
@@ -0,0 +1,31 @@
+'use strict'
+const {
+  Model
+} = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class Collection extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate (models) {
+      // define association here
+    }
+  }
+  Collection.init({
+    name: DataTypes.STRING,
+    category: DataTypes.STRING,
+    slogan: DataTypes.STRING,
+    artMaker: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    artRemark: DataTypes.STRING,
+    image: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Collection',
+    tableName:'Collections',
+    underscored: true
+  })
+  return Collection
+}
